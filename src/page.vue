7f6edf6bfde76d917b079ff16ffe236f38033d97
liaozhilan <lzl16975772022@163.com>
2025-06-24 11:08:54 +0800

    add photo

--- src/page.vue
+++ src/page.vue
@@ -2,7 +2,7 @@
  * @Author: liaozhilan lzl16975772022@163.com
  * @Date: 2025-06-24 09:28:37
  * @LastEditors: liaozhilan lzl16975772022@163.com
- * @LastEditTime: 2025-06-24 09:36:27
+ * @LastEditTime: 2025-06-24 11:07:45
  * @FilePath: \IL-photography\page.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -17,14 +17,14 @@
       >
         <!-- 图片 -->
         <img
-          :src="'https://via.placeholder.com/150?text=Image+' + (imgIndex + 1)"
-          alt="Image"
-          style="width: 100%; margin-bottom: 12px"
+          :src="require(`@/assets/photo/${imgIndex}.jpg`)"
+          :alt="'Image ' + imgIndex"
+          class="zoom-image"
         />
-
+        <!-- style="width: 100%; margin-bottom: 12px"  -->
         <!-- 每张图片对应 4 个评分 -->
         <div v-for="(question, qIndex) in 4" :key="qIndex" style="margin-bottom: 16px;">
-          <a-descriptions-item :label="'Q' + (qIndex + 1)" />
+          <span class="question-label">Q{{ qIndex + 1 }}</span>
           <a-slider
             v-model="ratings[imgIndex][qIndex]"
             :min="1"
@@ -60,6 +60,7 @@ export default {
   },
   methods: {
     submitForm() {
+      console.log('提交的评分:', this.ratings);
       // 你可以在这里发给后端或进行进一步处理
       // 示例：this.$axios.post('/api/submit', this.ratings)
     },
@@ -70,5 +71,13 @@ export default {
 <style scoped>
 #components-grid-demo-rating {
   padding: 24px;
+}
+.zoom-image {
+  width: 100%;
+  margin-bottom: 12px;
+  transition: transform 0.3s ease;
+}
+.zoom-image:hover {
+  transform: scale(1.1);
 }
 </style>
