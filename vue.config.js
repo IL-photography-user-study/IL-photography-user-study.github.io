7bf0a73c83ecab4bea184b4edd851d0e40771f57
liaozhilan <lzl16975772022@163.com>
2025-06-27 16:10:11 +0800

    rename2

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@
  * @Author: liaozhilan lzl16975772022@163.com
  * @Date: 2025-06-24 10:13:32
  * @LastEditors: liaozhilan lzl16975772022@163.com
- * @LastEditTime: 2025-06-27 15:48:36
+ * @LastEditTime: 2025-06-27 16:05:44
  * @FilePath: \IL-photography\vue.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = {
-  publicPath: "/IL-photography-user-study/"  
+  publicPath: '.'  
 }
