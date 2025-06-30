b8d2ac1cf6de4bf3fb2659e0b09ccf1adc0e9b9b
liaozhilan <lzl16975772022@163.com>
2025-06-30 15:02:00 +0800

    phone

--- src/page.vue
+++ src/page.vue
@@ -22,7 +22,16 @@
 
     <div v-for="row in 3" :key="'row-' + row">
       <a-row :gutter="16" class="group-wrapper">
-        <a-col :span="12" v-for="col in 2" :key="'group-' + ((row - 1) * 2 + col)">
+        <a-col 
+          :xs="24"
+          :sm="24"
+          :md="12"
+          :lg="12"
+          :xl="12"
+          :xxl="12" 
+          v-for="col in 2" 
+          :key="'group-' + ((row - 1) * 2 + col)"
+        >
           <template v-if="(row - 1) * 2 + col <= 6">
             <div class="group-title">Group {{ (currentGroup - 1) * 6 + (row - 1) * 2 + col }}</div>
 
